39de9d09e77c655b1e746c0c5594a38730b721ad
bagusnata330 <bagusnata330@gmail.com>
2020-10-13 22:06:23 +0800

    day 3 done

--- src/component/MainContent.js
+++ src/component/MainContent.js
@@ -3,10 +3,12 @@ import Header from "./Header";
 import ContactLogo from "./images/contactLogo.png";
 import PhoneLogo from "./images/phoneLogo.png";
 import EmailLogo from "./images/emailLogo.png";
+import GithubLogo from "./images/github.png";
 import LocationLogo from "./images/locLogo.png";
-import Nilai6 from "./images/bulet6.png";
-import Nilai7 from "./images/bulet7.png";
-import Nilai9 from "./images/bulet9.png";
+import Bulat1 from "./images/bulet1.png";
+import Bulat2 from "./images/bulet2.png";
+import Bulat4 from "./images/bulet4.png";
+import Bulat5 from "./images/bulet5.png";
 import Indonesia from "./images/indonesia.png";
 import Inggris from "./images/inggris.png";
 import Bali from "./images/bali.png";
@@ -14,23 +16,19 @@ import Game from "./images/Game.png";
 import TV from "./images/TV.png";
 import Music from "./images/Music.png";
 
-
-
-
 export class MainContent extends Component {
-
   render() {
-    const contactBox        = <ContactBox />;
-    const skillBox          = <SkillBox />;
-    const languageBox       = <LanguageBox />;
-    const passionBox        = <PassionBox />;
-    const aboutMe           = <AboutMe />;
-    const education         = <Education />;
-    const codingExperience  = <CodingExperience />
-    const project           = <Project />
+    const contactBox = <ContactBox />;
+    const skillBox = <SkillBox />;
+    const languageBox = <LanguageBox />;
+    const passionBox = <PassionBox />;
+    const aboutMe = <AboutMe />;
+    const education = <Education />;
+    const codingExperience = <CodingExperience />;
+    const project = <Project />;
 
     return (
-      <div style={{ marginBottom: "5px" }}>
+      <div>
         <div>
           <Header />
         </div>
@@ -66,12 +64,16 @@ function ContactBox() {
         Bagus Nata
       </div>
       <div className="font-logo">
-        <img className="contact-img" src={PhoneLogo} alt="" />
+        <img className="phone-img" src={PhoneLogo} alt="" />
         081-237-083-255
       </div>
       <div className="font-logo">
         <img className="email-img" src={EmailLogo} alt="" />
         Bagusnata330@gmail.com
+      </div>
+      <div className="font-logo">
+        <img className="github-img" src={GithubLogo} alt="" />
+        www.github.com/bagusnata330
       </div>
       <div className="font-logo">
         <img className="contact-img" src={LocationLogo} alt="" />
@@ -87,15 +89,24 @@ function SkillBox() {
       <h1 className="font-contact">SKILLS</h1>
       <div className="font-skill">
         Coding <br />
-        <img className="skill-img" src={Nilai6} alt="" />
+        <div style={{ display: "inline-block" }}>
+          <img className="skill-img" src={Bulat5} alt="" />
+          <img className="skill-img" src={Bulat1} alt="" />
+        </div>
       </div>
       <div className="font-skill">
         Renang <br />
-        <img className="skill-img" src={Nilai7} alt="" />
+        <div>
+          <img className="skill-img" src={Bulat5} alt="" />
+          <img className="skill2-img" src={Bulat2} alt="" />
+        </div>
       </div>
       <div className="font-skill">
         Rebahan <br />
-        <img className="skill-img" src={Nilai9} alt="" />
+        <div>
+          <img className="skill-img" src={Bulat5} alt="" />
+          <img className="skill-img" src={Bulat4} alt="" />
+        </div>
       </div>
     </div>
   );
@@ -104,7 +115,7 @@ function SkillBox() {
 function LanguageBox() {
   return (
     <div>
-      <h1 className="font-language-passion">LANGUAGE</h1>
+      <h1 className="font-language-header">LANGUAGE</h1>
       <div style={{ display: "inline-block" }}>
         <div className="font-language language-icon">
           <img className="language-img" src={Indonesia} alt="indonesia" />
@@ -124,11 +135,11 @@ function LanguageBox() {
 
 function PassionBox() {
   return (
-    <div style={{ paddingBottom: "20rem" }}>
-      <h1 className="font-language-passion">PASSIONS</h1>
+    <div style={{ paddingBottom: "4rem" }}>
+      <h1 className="font-passion-header">PASSIONS</h1>
       <div style={{ display: "inline-block" }}>
         <div className="font-passion passion-icon">
-          <img className="passion-img" src={Game} alt="Game logo" />
+          <img className="passionGame-img" src={Game} alt="Game logo" />
           <img className="passion-img" src={Music} alt="Music logo" />
           <img className="passion-img" src={TV} alt="TV logo" />
         </div>
@@ -144,13 +155,10 @@ function AboutMe() {
         <div className="aboutme-font">ABOUT ME</div>
       </div>
       <div className="aboutContent-font">
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem
+        Perkenalkan nama saya I Made Bagus Nata Dwi Kusuma, saya lahir di
+        Denpasar pada tanggal 30 Agustus 2002. Saya adalah anak bungsu dari 2
+        bersaudara. Saya berasal dari kabupaten Tabanan dan menetap tinggal di
+        Denpasar.
       </div>
     </div>
   );
@@ -163,13 +171,11 @@ function Education() {
         <div className="aboutme-font">EDUCATION</div>
       </div>
       <div className="aboutContent-font">
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem
+        Saya sudah menempuh pendidikan selama 12 tahun. Saya menempuh sekolah
+        dasar selama 6 tahun di SD Saraswati 6 Denpasar, lanjut ke SMP Negeri 12
+        Denpasar selama 3 tahun, setelah itu lanjut ke SMA Negeri 3 Denpasar
+        selama 3 tahun, dan sekarang saya sedang menempuh perguruan tinggi di
+        ITB Stikom Bali.
       </div>
     </div>
   );
@@ -182,13 +188,18 @@ function CodingExperience() {
         <div className="aboutme-font">EXPERIENCE</div>
       </div>
       <div className="aboutContent-font">
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem
+        Pengalaman saya mengenal dunia IT berawal dari kakak saya yang masuk ke
+        bidang IT, lalu ia menyarankan saya untuk mengambil kuliah jurusan IT
+        karena kedepannya IT pasti sangat dibutuhkan di dunia kerja. Ia
+        menyarankan saya masuk ke ITB Stikom Bali yang mana dia adalah alumni
+        dari sana. Saya pertama kali diajarkan coding pada saat lulus SMA, saya
+        diajarkan bahasa pemrogramman javascript dengan menggunakan libraries
+        react. Hari-hari pertama memang terasa agak sulit untuk memahami semua
+        pelajarannya karena saya sama sekali belum mengetahui tentang bahasa
+        pemrogramman. Dalam waktu 4 bulan yaitu saat libur kelulusan SMA saya
+        terus belajar untuk lebih memahami bahasa pemrogramman ini, dan sekarang
+        saya sudah lumayan mengerti bagaimana cara coding meskipun masih banyak
+        sekali yang saya belum ketahui.
       </div>
     </div>
   );
@@ -200,12 +211,32 @@ function Project() {
       <div className="project-header">
         <div className="aboutme-font">PROJECT</div>
       </div>
-      <h3 className="text-right">Binary Converter</h3>
-      <p>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s
-      </p>
+      <div>
+        <h3 className="projectList-font">&#10148; Binary Converter</h3>
+        <p className="aboutContent-font">
+          Dalam project ini saya membuat converter yaitu dari decimal ke binary.
+          Binary merupakan bahasa yang dimengerti komputer yang terdiri dari 0
+          dan 1.
+        </p>
+      </div>
+      <div>
+        <h3 className="projectList-font"> &#10148; Shoes Store</h3>
+        <p className="aboutContent-font">
+          Dalam project ini saya belajar bagaimana cara menambah barang ke
+          keranjang belanjaan, mentotal semua harga barang di keranjang, dan
+          membuat form yang diisi data produk yang ingin dimasukkan ke list
+          produk. Di github saya ada 2 project ini yang saya beri nama
+          KrenengShoeStore-app, Belajar-app (KrenengShoesStore yang saya buat
+          full stack).
+        </p>
+      </div>
+      <div>
+        <h3 className="projectList-font">&#10148; Sorting Algorithm</h3>
+        <p className="aboutContent-font">
+          Dalam project ini saya belajar bagaimana cara membuat sorting
+          algorithm menggunakan javascript.
+        </p>
+      </div>
     </div>
   );
 }
